48824ee8052c3f2194ad27da6606644241f0d238
mv28jam <mv28jam@yandex.ru>
2023-04-20 17:17:12 +0300

    shah add

--- src/Game.php
+++ src/Game.php
@@ -118,14 +118,16 @@ class Game
             $this->animated_output->deleteLine();
             $this->animated_output->cursorUp();
             $this->animated_output->echoLine($this->input_move);
+        } catch (GameMechanicsException $e) {
+            $this->animated_output->echoMultipleLine($this->desk->dump(), 1);
+            $this->animated_output->deleteLine();
+            $this->animated_output->echoLine($e->getMessage());
+            $this->animated_output->cursorUp();
+            $this->animated_output->echoLine($this->input_move);
         } catch (EndGameException $e) {
             $this->animated_output->echoLine($e->getMessage() . (new Move($move)));
             $this->animated_output->echoEmptyLine();
             exit(0);
-        } catch (GameMechanicsException $e) {
-            $this->animated_output->echoLine($e->getMessage());
-            $this->animated_output->cursorUp();
-            $this->animated_output->echoLine($this->input_move);
         } catch (\Exception $e) {
             //do not save move and echo error message
             $this->animated_output->echoLine($this->mistake . $e->getMessage());
