c58bba9082cacede515a39f1cf07a6e762eb1347
mv28jam <mv28jam@yandex.ru>
2023-04-20 14:05:08 +0300

    minor

---
+++ src/GameMechanics.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/**
+ * GameMehanics simulates and check moves
+ * @TODO
+ * @author mv28jam
+ */
+class GameMechanics
+{
+
+    public function isFieldUnderAttack(bool $is_black, array $field, Desk $desk)
+    {
+
+    }
+
+    public function isKingUnderAttack(bool $is_black, Desk $desk)
+    {
+
+    }
+
+}
